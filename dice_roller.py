e7fe0cee2a3795b5dd426b0d21eb6a6f781c3ba8
robertlato79 <robertlato@gmail.com>
2021-04-02 20:55:00 +0200

    Now rolling three dice and provide sum of rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,8 @@
 import random 
-
-def main():
+dice_rolls = 3
+dice_sum = 0
+for i in range(0,dice_rolls): 
   roll = random.randint(1,6)
+  dice_sum += roll
   print(f'You rolled a {roll}')
-
-if __name__== "__main__":
-  main()
+print(f'You have rolled a total of {dice_sum}')
